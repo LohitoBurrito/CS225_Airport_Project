dae34e39dc08ae7ccd6020e58bad4c5079e28e8c
LohitoBurrito <110351173+LohitoBurrito@users.noreply.github.com>
2022-11-30 15:04:58 -0600

    Update main.cpp

--- Airport Project/main.cpp
+++ Airport Project/main.cpp
@@ -27,32 +27,86 @@ int main() {
 
     Airports system = Airports(currLat_, currLong_, destLat_, destLong_, baggageAmount);
 
-    //Check if there is a closed airport
+    //Closed Airport Check
     for (int i = 0; i < system.smallAirports.size(); i++) {
         if (system.smallAirports[i]->type == "closed") {
             std::cout << "Bruh" << " small" << "\n";
             return 0;
         }
     }
-    std::cout << "Checked Small Airports"<< system.smallAirports.size() << "\n";
+    std::cout << "Checked " << system.smallAirports.size() << " Small Airports" << "\n";
     for (int i = 0; i < system.medAirports.size(); i++) {
         if (system.medAirports[i]->type == "closed") {
             std::cout << "Bruh" << " mediums" << "\n";
             return 0;
         }
     }
-    std::cout << "Checked Medium Airports" << system.medAirports.size() << "\n";
+    std::cout << "Checked "<<system.medAirports.size()<<" Medium Airports" << "\n";
     for (int i = 0; i < system.largeAirports.size(); i++) {
         if (system.largeAirports[i]->type == "closed") {
             std::cout << "Bruh" << " large" << "\n";
             return 0;
         }
     }
-    std::cout << "Checked Large Airports" << system.largeAirports.size()<< "\n";
+    std::cout << "Checked " << system.largeAirports.size() << " Large Airports" << "\n";
 
-    //Haversine test cases
-    cout << system.calcDistance(41.97859955, -87.90480042, 25.273056, 51.608056) << "\n"; //CHICAGO -> DOHA
-    //Calc Cost test cases
-    cout << system.calcCost(41.97859955, -87.90480042, 25.273056, 51.608056, "large_airport") << "\n"; //CHICAGO -> DOHA
+    //Chicago -> Doha
+    try {
+        double dist = system.calcDistance(41.97859955, -87.90480042, 25.273056, 51.608056);
+        if (abs(dist - 11479) > 50) {
+            throw (dist);
+        }
+        else {
+            cout << "CHICAGO -> DOHA Distance is valid\n";
+        }
+    }
+    catch(int num) {
+        cout << "CHICAGO -> DOHA distance in km:\nCalculated Distance: "<<num<<"km\nEstimated Distance: 11479km\n";
+        cout << "Haversine Distance too far out\n";
+    }
+    try {
+        double cost = system.calcCost(41.97859955, -87.90480042, 25.273056, 51.608056, "large_airport");
+        double expediaCost = 2778.38 / 2;
+        if (abs(cost - expediaCost) > 400) {
+            throw(cost);
+        }
+        else {
+            cout << "CHICAGO -> DOHA Cost is valid\n";
+        }
+    }
+    catch (int num) {
+        cout << "CHICAGO -> DOHA cost ($):\nCalculated Cost: $" << num << "\nEstimated Cost: $"<<2778.38/2<<"\n";
+        cout << "Calculated cost is a little too far off\n";
+    }
+
+    //Chicago -> Frankfurt
+    try {
+        double dist = system.calcDistance(41.97859955, -87.90480042, 50.026402, 8.54313);
+        if (abs(dist - 6989) > 50) {
+            throw (dist);
+        }
+        else {
+            cout << "CHICAGO -> FRANKFURT Distance is valid\n";
+        }
+    }
+    catch (int num) {
+        cout << "CHICAGO -> FRANKFURT distance in km:\nCalculated Distance: " << num << "km\nEstimated Distance: 6989km\n";
+        cout << "Haversine Distance too far out\n";
+    }
+    try {
+        double cost = system.calcCost(41.97859955, -87.90480042, 50.026402, 8.54313, "large_airport");
+        double expediaCost = 2276 / 2;
+        if (abs(cost - expediaCost) > 400) {
+            throw(cost);
+        }
+        else {
+            cout << "CHICAGO -> FRANKFURT Cost is valid\n";
+        }
+    }
+    catch (int num) {
+        cout << "CHICAGO -> FRANKFURT cost ($):\nCalculated Cost: $" << num << "\nEstimated Cost: $" << 2276 / 2 << "\n";
+        cout << "Calculated cost is a little too far off\n";
+    }
+
     return 0;    
 }
